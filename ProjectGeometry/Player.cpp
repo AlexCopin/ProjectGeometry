e4cce77d1bc3bd354d73a90fb6bacfeec1abe453
Vincent <vincentboutin.dev@gmail.com>
2020-11-11 18:40:22 +0100

    Fonctions player cpp Tir + cadence + death + takedamages

--- ProjectGeometry/Player.cpp
+++ ProjectGeometry/Player.cpp
@@ -6,6 +6,11 @@ Player* CreatePlayer(int life, int speed, int posX, int posY) {
 	player->speedP = speed;
 	player->posX = posX;
 	player->posY = posY;
+	sf::Vector2f PosPlayer(posX, posY);
+	player->playerShape.setPosition(PosPlayer);
+	player->playerShape.setRadius(2.0f);
+	player->playerShape.setFillColor(sf::Color::White);
+
 	return player;
 }
 
@@ -25,4 +30,38 @@ void MovePlayer(Player* player, bool up, bool right, bool down, bool left) {
 	}
 	return;
 
+}
+
+float GetTime() {
+	float timeInSeconds = clock() / (float)CLOCKS_PER_SEC;
+	return timeInSeconds;
+}
+
+//Preciser float time = GetTime() en debut de jeu 
+bool canFire(float& time, float cadence) {
+	if (GetTime() < time + cadence) {
+		return false;
+	}
+	else {
+		time = GetTime();
+		return true;
+	}
+}
+
+void Fire(Player* player, bool canFire, int damages, int posX, int posY) {
+	if (canFire) {
+		//CreateBullet(damages, player->weapon, posX, posY); 
+	}
+
+}
+
+void TakeDamage(Player* player, int damages) {
+	player->lifeP -= damages;
+}
+
+
+void PlayerDeath(Player* player) {
+	if (player->lifeP <= 0) {
+		delete player;
+	}
 }
--- ProjectGeometry/Player.h
+++ ProjectGeometry/Player.h
@@ -11,6 +11,7 @@ struct Player
 	int speedP = 0;
 	sf::CircleShape playerShape;
 	Ship **playerShips;
+
 	int posX = 0;
 	int posY = 0;
 };
@@ -18,3 +19,9 @@ struct Player
 Player* CreatePlayer(int life, int speed, int posX, int posY);
 
 void MovePlayer(Player* player, bool up, bool right, bool down, bool left);
+float GetTime();
+bool canFire(float& time, float cadence);
+void Fire(Player* player, bool canFire, int damages, int posX, int posY);
+void TakeDamage(Player* player, int damages);
+void PlayerDeath(Player* player);
+
