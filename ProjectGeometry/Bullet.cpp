2291ad2d05c466ef7fc336b272f8a13229bf3405
Alex Copin <alexcopin@live.fr>
2020-11-11 20:05:04 +0100

    Auto stash before merge of "main" and "origin/main"

--- ProjectGeometry/Bullet.cpp
+++ ProjectGeometry/Bullet.cpp
@@ -1,8 +1,43 @@
 #include "Bullet.h"
 
 
-Bullet* CreateBullet(int damage, int speed, TYPEBULLET typeShape)
+Bullet* CreateBullet(int damage, TYPEBULLET typeShape, int posX, int posY)
 {
 	Bullet* bullet = new Bullet;
+	std::size_t count = 0;	
+	if(typeShape == TYPEBULLET::STICK)
+	{
+		count = 2;
+	}else if(typeShape == TYPEBULLET::TRIANGLE)
+	{
+		count = 3;
+	}
+	else if (typeShape == TYPEBULLET::SQUARE)
+	{
+		count = 4;
+	}
+	else if (typeShape == TYPEBULLET::PENTAGONE)
+	{
+		count = 5;
+	}
+	else if (typeShape == TYPEBULLET::HEXAGONE)
+	{
+		count = 6;
+	}
+	bullet->originPosX = posX;
+	bullet->originPosY = posY;
+	bullet->damageB = damage;
+	bullet->typeB = typeShape;
+	bullet->shapeB.setPointCount(count);
+	bullet->shapeB.setPosition(posX, posY);
 	return bullet;
+}
+
+void MoveBullet(int speed, sf::Mouse mouse, Bullet& bullet, sf::Window window)
+{
+	sf::Vector2i posMouse = mouse.getPosition(window);
+	sf::Vector2i originPosBullet(bullet.originPosX, bullet.originPosY);
+	sf::Vector2i trajectoireBullet(posMouse - originPosBullet);
+	float timeInSeconds = clock() / (float)CLOCKS_PER_SEC;
+	bullet.shapeB.setPosition(trajectoireBullet.x * (timeInSeconds * speed), trajectoireBullet.y *(timeInSeconds * speed));
 }
--- ProjectGeometry/Bullet.h
+++ ProjectGeometry/Bullet.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include <SFML/Graphics.hpp>
+#include <time.h>
 enum class TYPEBULLET
 {
 	STICK,
@@ -12,8 +13,13 @@ struct Bullet
 {
 	int damageB = 0;
 	float speedB = 0.0f;
-	TYPEBULLET typeB;
+	TYPEBULLET typeB = TYPEBULLET::STICK;
+	sf::CircleShape shapeB;
+	int originPosX = 0;
+	int originPosY = 0;
 };
 
 
-Bullet* CreateBullet(int damage, int speed, TYPEBULLET typeShape);
+Bullet* CreateBullet(int damage, TYPEBULLET typeShape, int posX, int posY);
+
+void MoveBullet(int speed, int posMouseX, int posMouseY, sf::Window window);
