f597fa1f377b0cfcc98837dcb51da93ca0dc0793
Vincent <vincentboutin.dev@gmail.com>
2020-11-11 17:16:14 +0100

    function player and movement

--- ProjectGeometry/Player.cpp
+++ ProjectGeometry/Player.cpp
@@ -1,1 +1,28 @@
 #include"Player.h"
+
+Player* CreatePlayer(int life, int speed, int posX, int posY) {
+	Player* player = new Player();
+	player->lifeP = life;
+	player->speedP = speed;
+	player->posX = posX;
+	player->posY = posY;
+	return player;
+}
+
+void MovePlayer(Player* player, bool up, bool right, bool down, bool left) {
+
+	if (up) {
+		player->posY++;
+	}
+	else if (right) {
+		player->posX++;
+	}
+	else if (down) {
+		player->posY--;
+	}
+	else if (left) {
+		player->posX--;
+	}
+	return;
+
+}
--- ProjectGeometry/Player.h
+++ ProjectGeometry/Player.h
@@ -11,4 +11,10 @@ struct Player
 	int speedP = 0;
 	sf::CircleShape playerShape;
 	Ship **playerShips;
+	int posX = 0;
+	int posY = 0;
 };
+
+Player* CreatePlayer(int life, int speed, int posX, int posY);
+
+void MovePlayer(Player* player, bool up, bool right, bool down, bool left);
