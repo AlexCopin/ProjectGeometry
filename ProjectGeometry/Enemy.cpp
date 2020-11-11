f09ca7d95e30eae207b49b9c070297f16bc9982c
Vincent <vincentboutin.dev@gmail.com>
2020-11-11 19:51:35 +0100

    Debut enemy

--- ProjectGeometry/Enemy.cpp
+++ ProjectGeometry/Enemy.cpp
@@ -1,1 +1,15 @@
 #include "Enemy.h"
+
+Enemy* CreateEnemy(int life, int speed, int posX, int posY) {
+	Enemy* enemy = new Enemy();
+	enemy->lifeE = life;
+	enemy->speedE = speed;
+	enemy->posX = posX;
+	enemy->posY = posY;
+	sf::Vector2f PosEnemy(posX, posY);
+	//enemy->playerShape.setPosition(PosPlayer);
+	//enemy->playerShape.setRadius(2.0f);
+	//enemy->playerShape.setFillColor(sf::Color::White);
+
+	return enemy;
+}
--- ProjectGeometry/Enemy.h
+++ ProjectGeometry/Enemy.h
@@ -1,2 +1,25 @@
 #pragma once
 #include "Bullet.h"
+#include <SFML/Graphics.hpp>
+
+enum class TYPEBULLET
+{
+	STICK,
+	TRIANGLE,
+	SQUARE,
+	PENTAGONE,
+	HEXAGONE
+};
+
+struct Enemy
+{
+	int lifeE = 0;
+	int speedE = 0;
+	//sf::Shape enemyShape;
+	TYPEBULLET enemyType;
+
+	int posX = 0;
+	int posY = 0;
+};
+
+Enemy* CreateEnemy(int life, int speed, int posX, int posY);
