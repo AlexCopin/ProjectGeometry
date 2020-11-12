03d5093f40ad3579a27718ed2e45064261be6f1d
Alex Copin <alexcopin@live.fr>
2020-11-12 13:49:55 +0100

    Player Changes

--- ProjectGeometry/Player.h
+++ ProjectGeometry/Player.h
@@ -12,13 +12,13 @@ public:
 	int posX = 0;
 	int posY = 0;
 	Player *CreatePlayer(int life, float speed, int posX, int posY);
-	void MovePlayer(Player& player, std::string direction);
-	void RotatePlayer(Player *player, sf::RenderWindow *window);
+	void MovePlayer(std::string direction);
+	void RotatePlayer(sf::RenderWindow *window);
 	float GetTime();
 	bool canFire(float &time, float cadence);
-	void Fire(Player *player, bool canFire, int damages, int posX, int posY);
-	void TakeDamage(Player *player, int damages);
-	void PlayerDeath(Player *player);
+	void Fire(bool canFire, int damages, int posX, int posY);
+	void TakeDamage(int damages);
+	void PlayerDeath();
 	void Start(sf::RenderWindow* window);
 	void Update(sf::RenderWindow* window, float deltaTime);
 };
