403dcb450778c0bd6f3e13e8a54941f2694022d6
jqntn <jqntn88@gmail.com>
2020-11-16 17:57:45 +0100

    updating enemies

--- ProjectGeometry/Enemy.h
+++ ProjectGeometry/Enemy.h
@@ -3,17 +3,28 @@ class Player;
 class Enemy : Object
 {
 public:
-	enum Type
+	enum class Type
 	{
 		Triangle,
 		Square,
 		Octagon,
 		Circle
 	} type;
-	sf::CircleShape shape;
 	sf::Vector2f position;
-	uint16_t health, speed;
+	uint16_t health;
+	float speed;
+	// Appearance
+	sf::CircleShape shape;
+	sf::Color color;
+	float radius;
+	// Player
 	Player *player;
+	// Target
+	sf::Vector2f target;
+	bool isDirVert;
+	float patrolTime = 2;
+	float timer = patrolTime;
+	float isOnTarget = 1;
 	Enemy(std::string id, sf::Vector2f position, Type type);
 	void Update(sf::RenderWindow *window, float deltaTime);
 };
